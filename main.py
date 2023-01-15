447029238a25e40296aee70b6560944f53d9e2e2
David Venegas <venegasdavidm@gmail.com>
2023-01-15 00:15:40 -0500

    need twitter elevated access

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 import pip._vendor.requests as requests
 import tweepy
 
-twitter_api_key = "K3sDjWRTgZWIYAvb2dBKsn1xm"
-twitter_api_key_secret = "oak5WmF2KUT82W2rWn4swdOiCy39IfhyqU0Mhv4JUN2gylu1fg"
-twitter_bearer_token = "AAAAAAAAAAAAAAAAAAAAAJTplAEAAAAAz3jQP2VVkt26YMxqA1UvyK%2FHhxw%3DS4xAlGv8VTIgjhauofaTwJMcBPKs7NGSbluIvjPkpRygvtJFDz"
+def get_image():
+    images_request = requests.get('https://dalle2.gallery/api/images/aggregated?pagesize=20&page=0&random=true', verify=False)
+    images_as_json = images_request.json()
+    image_with_link = next(item for item in images_as_json if item['Caption'] is not None and item['PromptImagePath'] is not None)
 
-twitter_access_token = "1614455148125749250-yaiyhsYENkqgsK7l6ZgMjjogOwjXyl"
-twitter_access_token_secret = "o9hHpOgWmYsGO90TX9U92JtWf4ynntwRFGsjbvi4NLzEz"
-
-images_request = requests.get('https://dalle2.gallery/api/images/aggregated?pagesize=20&page=0&random=true', verify=False)
-images_as_json = images_request.json()
-image_with_link = next(item for item in images_as_json if item['Caption'] is not None and item['PromptImagePath'] is not None)
+    image_caption = image_with_link['Caption']
+    image_URL = image_with_link['PromptImagePath']
+    return image_caption, image_URL
 
 def api():
-    # auth = 
-    print('Yo')
+    auth = tweepy.OAuthHandler("K3sDjWRTgZWIYAvb2dBKsn1xm", "oak5WmF2KUT82W2rWn4swdOiCy39IfhyqU0Mhv4JUN2gylu1fg")
+    auth.set_access_token("1614455148125749250-yaiyhsYENkqgsK7l6ZgMjjogOwjXyl", "o9hHpOgWmYsGO90TX9U92JtWf4ynntwRFGsjbvi4NLzEz")
+    return tweepy.API(auth)
 
-print(image_with_link)
+
+def tweet(api: tweepy.API, message: str, image_path=None):
+    if image_path:
+        api.update_status_with_media(message, image_path)
+    else:
+        api.update_status(message)
+
+    print('Tweeted successfully!')
+ 
+
+image_caption, image_URL = get_image()
+
+print(image_caption)
+print(image_URL)
+
+tweet(api(), 'This is my first tweet as a bot, Hello World')
